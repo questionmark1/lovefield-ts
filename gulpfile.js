54bbc9c760cd8ca3ea7b4b5a2c102bafb8cc2035
Arthur Hsu <arthurhsu@chromium.org>
2015-09-24 10:23:25 -0700

    Fix file formatting

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,6 @@ var gulp = require('gulp');
 var log = console.log.bind(console);
 
 gulp.task('default', function() {
-	log('Usage:');
-	log('  gulp build: build');
+  log('Usage:');
+  log('  gulp build: build');
 });
--- lib/base/enum.ts
+++ lib/base/enum.ts
@@ -1,38 +1,38 @@
 module lf {
-	export enum ConstraintAction {
-		RESTRICT,
-		CASCADE
-	}
-	
-	export enum ConstraintTiming {
-		IMMEDIATE,
-		DEFERRABLE
-	}
-	
-	export enum Order {
-		DESC,
-		ASC
-	}
-	
-	export enum Type {
-		ARRAY_BUFFER,
-		BOOLEAN,
-		DATE_TIME,
-		INTEGER,
-		NUMBER,
-		STRING,
-		OBJECT		
-	}
-	
-	module type {
-		export var DEFAULT_VALUES = {
-			0: null,  // lf.Type.ARRAY_BUFFER
+  export enum ConstraintAction {
+    RESTRICT,
+    CASCADE
+  }
+
+  export enum ConstraintTiming {
+    IMMEDIATE,
+    DEFERRABLE
+  }
+
+  export enum Order {
+    DESC,
+    ASC
+  }
+
+  export enum Type {
+    ARRAY_BUFFER,
+    BOOLEAN,
+    DATE_TIME,
+    INTEGER,
+    NUMBER,
+    STRING,
+    OBJECT
+  }
+
+  module type {
+    export var DEFAULT_VALUES = {
+      0: null,  // lf.Type.ARRAY_BUFFER
       1: false,  // lf.Type.BOOLEAN
       2: Object.freeze(new Date(0)),  // lf.Type.DATE_TIME
       3: 0,  // lf.Type.INTEGER
       4: 0,  // lf.Type.NUMBER
       5: '',  // lf.Type.STRING
       6: null  // lf.Type.OBJECT
-		}
-	}
+    }
+  }
 }
