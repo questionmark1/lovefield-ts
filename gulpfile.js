e085e2e86cdde3d2ccaf81d3e1b936a285b3b177
arthurhsu <artchsu@gmail.com>
2015-10-17 09:27:11 -0700

    Another try for gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var fs = require('fs-extra');
 var path = require('path');
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var ts = require('gulp-typescript');
 var tsd = require('gulp-tsd');
 var tslint = require('gulp-tslint');
+var sourcemaps = require('gulp-sourcemaps');
 var webserver = require('gulp-webserver');
 var nopt = require('nopt');
 var merge = require('merge-stream');
@@ -47,6 +49,31 @@ function getFolders(dir) {
 }
 
 
+var COMPILER_OPTIONS = {
+  declaration: true,
+  module: 'commonjs',
+  noEmitOnError: true,
+  noExternalResolve: true,
+  noImplicitAny: true,
+  sortOutput: true  
+};
+
+
+function buildLib() {
+  var tsResults =
+      gulp.src('lib/**/*.ts')
+          .pipe(sourcemaps.init())
+          .pipe(ts(COMPILER_OPTIONS));
+  return merge([
+    tsResults.dts.pipe(gulp.dest('out/definitions')),
+    tsResults.js
+        .pipe(concat('lf.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('out/js'))    
+  ]);
+}
+
+
 gulp.task('build', ['tsd'], function() {
   var knownOpts = {
     'target': ['lib', 'tests']
@@ -58,30 +85,33 @@ gulp.task('build', ['tsd'], function() {
   }
 
   if (target == 'lib') {
-    return gulp.src('lib/**/*.ts').
-            pipe(ts({
-              module: 'commonjs',
-              noEmitOnError: true,
-              noImplicitAny: true,
-              out: 'lovefield.js'
-            })).js.pipe(gulp.dest('out'));
-  } else {
-    // Not compiling yet, the definition of lf needs to be wired out correctly
-    var tests = getFolders('tests');
-    console.log(tests);
-    var tasks = tests.map(function(folder) {
-      return gulp.src([
-        'lib/**/*.ts',
-        'tests/' + folder + '/**/*.ts'
-      ]).pipe(ts({
-        noEmitOnError: true,
-        noImplicitAny: true,
-        out: folder + '_test.js'
-      })).js.pipe(gulp.dest('out/' + folder));
-    });
-
-    return merge(tasks);
+    return buildLib();
   }
+//   if (target == 'lib') {
+//     return gulp.src('lib/**/*.ts').
+//             pipe(ts({
+//               module: 'commonjs',
+//               noEmitOnError: true,
+//               noImplicitAny: true,
+//               out: 'lf.js'
+//             })).js.pipe(gulp.dest('out'));
+//   } else {
+//     // Not compiling yet, the definition of lf needs to be wired out correctly
+//     var tests = getFolders('tests');
+//     console.log(tests);
+//     var tasks = tests.map(function(folder) {
+//       return gulp.src([
+//         'lib/**/*.ts',
+//         'tests/' + folder + '/**/*.ts'
+//       ]).pipe(ts({
+//         noEmitOnError: true,
+//         noImplicitAny: true,
+//         out: folder + '_test.js'
+//       })).js.pipe(gulp.dest('out/' + folder));
+//     });
+// 
+//     return merge(tasks);
+//   }
 });
 
 
