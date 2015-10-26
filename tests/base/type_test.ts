bb12696fd1c7d19d8b2da966430600f6bae42b05
arthurhsu <artchsu@gmail.com>
2015-10-25 22:20:03 -0700

    Fix lint issues.

--- tests/base/type_test.ts
+++ tests/base/type_test.ts
@@ -4,15 +4,12 @@ import * as lf from '../../out/dist/lf';
 import {runTest} from '../run';
 
 describe('Test default values', () => {
-it('Browsers should not complain about default values', () => {
+  it('Browsers should not complain about default values', () => {
     var clone: any[] = [];
-    lf.type.DEFAULT_VALUES.forEach(function(element) {
+    lf.type.DEFAULT_VALUES.forEach(function(element: any) {
       clone.push(element);
     });
     chai.expect(clone.length).to.be.gt(0);
-    var p = new Promise(function(resolve, reject) {
-      resolve();
-    });
   });
   runTest();
 });
